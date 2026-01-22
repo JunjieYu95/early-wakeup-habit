7dec623687b67d09d4c2d8f379536148c4ef5ac4
junjieyu <junjieyu1001@gmail.com>
2026-01-21 23:11:04 -0700

    Fix Vite outDir to output within apps/web directory

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: resolve(__dirname, "../../dist"),
-    emptyOutDir: true
+    outDir: "./dist", // or just 'dist'
   },
   server: {
     proxy: {
